d5004e9de8f28cc4a8c567571f84bf53a8ddb06a
Matt Carter <m@ttcarter.com>
2016-02-19 12:03:22 +1000

    Initial query tests passing

---
+++ test/query.js
@@ -0,0 +1,43 @@
+var expect = require('chai').expect;
+var mongoloid = require('..');
+var testSetup = require('./setup');
+
+describe('Mongoloid - query', function() {
+	before(testSetup.init);
+
+	it('should query the users model', function(finish) {
+		mongoloid.query({
+			$collection: 'users',
+			$sort: 'name',
+		}, function(err, users) {
+			expect(err).to.not.be.ok;
+			expect(users).to.be.an.array;
+
+			expect(users[0]).to.have.property('name', 'Jane Quark');
+			expect(users[0]).to.have.property('role', 'user');
+			expect(users[0]).to.have.property('favourite');
+			expect(users[0].favourite).to.be.a.string;
+			expect(users[0]).to.have.property('mostPurchased');
+			expect(users[0].mostPurchased).to.be.an.array;
+			expect(users[0].mostPurchased).to.have.length(2);
+			expect(users[0].mostPurchased[0]).to.have.property('number', 1);
+			expect(users[0].mostPurchased[0].item).to.be.a.string;
+			expect(users[0].mostPurchased[1]).to.have.property('number', 2);
+			expect(users[0].mostPurchased[1].item).to.be.a.string;
+
+			expect(users[1]).to.have.property('name', 'Joe Random');
+			expect(users[1]).to.have.property('role', 'user');
+			expect(users[1]).to.have.property('favourite');
+			expect(users[1].mostPurchased).to.be.an.array;
+			expect(users[1].mostPurchased).to.have.length(3);
+			expect(users[1].mostPurchased[0]).to.have.property('number', 5);
+			expect(users[1].mostPurchased[0].item).to.be.a.string;
+			expect(users[1].mostPurchased[1]).to.have.property('number', 10);
+			expect(users[1].mostPurchased[1].item).to.be.a.string;
+			expect(users[1].mostPurchased[2]).to.have.property('number', 15);
+			expect(users[1].mostPurchased[2].item).to.be.a.string;
+
+			finish();
+		});
+	});
+});
---
+++ test/setup.js
@@ -0,0 +1,146 @@
+var async = require('async-chainable');
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var scenario = require('mongoose-scenario');
+
+var hasSetup = false;
+
+module.exports = {
+	// init {{{
+	init: function(finish) {
+		var self = module.exports;
+		if (hasSetup) return finish();
+
+		async()
+			.then(self.initConnection)
+			.then(self.initSchemas)
+			.then(self.initScenarios)
+			.end(function(err) {
+				if (err) return finish(err);
+				hasSetup = true;
+				finish();
+			});
+	},
+	// }}}
+
+	// initConnection {{{
+	initConnection: function(finish) {
+		mongoose.connect('mongodb://localhost/mongoloid-test', finish);
+		mongoose.connection.on('error', console.error.bind(console, 'DB connection error:'));
+	},
+	// }}}
+
+	// initSchemas {{{
+	initSchemas: function(finish) {
+		// User {{{
+		var userSchema = new mongoose.Schema({
+			id: mongoose.Schema.ObjectId,
+			name: String,
+			role: {type: String, enum: ['user', 'admin'], default: 'user'},
+			favourite: {type: mongoose.Schema.ObjectId, ref: 'widgets'},
+			items: [{type: mongoose.Schema.ObjectId, ref: 'widgets'}],
+			mostPurchased: [
+				{
+					number: {type: Number, default: 0},
+					item: {type: mongoose.Schema.ObjectId, ref: 'widgets'},
+				}
+			],
+		});
+		var User = mongoose.model('users', userSchema);
+		// }}}
+
+		// Widget {{{
+		var widgetSchema = new mongoose.Schema({
+			id: mongoose.Schema.ObjectId,
+			name: String,
+			content: String,
+			status: {type: String, enum: ['active', 'deleted'], default: 'active'},
+		});
+		var Widget = mongoose.model('widgets', widgetSchema);
+		// }}}
+
+		// Group {{{
+		var groupSchema = new mongoose.Schema({
+			id: mongoose.Schema.ObjectId,
+			name: String,
+			preferences: {
+				defaults: {
+					items: [{type: mongoose.Schema.ObjectId, ref: 'widgets'}]
+				}
+			},
+		});
+		var Group = mongoose.model('groups', groupSchema);
+		// }}}
+
+		finish();
+	},
+	// }}}
+
+	// initScenarios {{{
+	initScenarios: function(finish) {
+		scenario.import({
+			users: [
+				{
+					name: 'Joe Random',
+					role: 'user',
+					favourite: 'widget-crash',
+					items: ['widget-bang'],
+					mostPurchased: [
+						{
+							number: 5,
+							item: 'widget-crash',
+						},
+						{
+							number: 10,
+							item: 'widget-bang',
+						},
+						{
+							number: 15,
+							item: 'widget-whollop',
+						},
+					],
+				},
+				{
+					name: 'Jane Quark',
+					role: 'user',
+					favourite: 'widget-bang',
+					items: [],
+					mostPurchased: [
+						{
+							number: 1,
+							item: 'widget-bang',
+						},
+						{
+							number: 2,
+							item: 'widget-whollop',
+						},
+					],
+				},
+			],
+			widgets: [
+				{
+					_ref: 'widget-crash',
+					name: 'Widget crash',
+					content: 'This is the crash widget',
+				},
+				{
+					_ref: 'widget-bang',
+					name: 'Widget bang',
+					content: 'This is the bang widget',
+				},
+				{
+					_ref: 'widget-whollop',
+					name: 'Widget whollop',
+					content: 'This is the whollop widget',
+				}
+			]
+		}, {
+			connection: mongoose.connection,
+			nuke: true,
+		}, function(err, data) {
+			expect(err).to.be.not.ok;
+			finish();
+		});
+	},
+	// }}}
+};
