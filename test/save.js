ff77f959077fd60bae14fcf4d15760a608e02e55
Matt Carter <m@ttcarter.com>
2016-04-06 14:48:41 +1000

    Extended saving test to also check OID saving

--- test/save.js
+++ test/save.js
@@ -19,14 +19,27 @@ describe('Monoxide - save', function() {
 		});
 	});
 
+	var widgets;
+	it('should get a list of existing widgets', function(finish) {
+		monoxide.query({
+			$collection: 'widgets',
+			$sort: 'name',
+		}, function(err, res) {
+			expect(err).to.be.not.ok;
+			expect(res).to.be.an.array;
+			widgets = res;
+			finish();
+		});
+	});
+
 	it('should save a user', function(finish) {
 		monoxide.save({
 			$collection: 'users',
 			$id: users[0]._id,
 			name: 'Edited User',
 			mostPurchased: [
-				{number: 12},
-				{number: 15},
+				{number: 12, item: widgets[0]._id.toString()},
+				{number: 15, item: widgets[1]._id.toString()},
 			],
 		}, function(err, user) {
 			expect(err).to.not.be.ok;
@@ -40,21 +53,23 @@ describe('Monoxide - save', function() {
 			expect(user.mostPurchased).to.be.an.array;
 			expect(user.mostPurchased).to.have.length(2);
 			expect(user.mostPurchased[0]).to.have.property('number', 12);
-			expect(user.mostPurchased[0].item).to.be.undefined;
+			expect(user.mostPurchased[0]).to.have.property('item', widgets[0]._id);
+			expect(user.mostPurchased[0].item).to.be.a.string;
 			expect(user.mostPurchased[1]).to.have.property('number', 15);
-			expect(user.mostPurchased[1].item).to.be.undefined;
+			expect(user.mostPurchased[1]).to.have.property('item', widgets[1]._id);
+			expect(user.mostPurchased[1].item).to.be.a.string;
 
 			finish();
 		});
 	});
 
 	it('should save a user (via model)', function(finish) {
-		users[0].name = 'Edited User2';
-		users[0].mostPurchased = [
-			{number: 12},
-			{number: 15},
+		users[1].name = 'Edited User2';
+		users[1].mostPurchased = [
+			{number: 18, item: widgets[1]._id.toString()},
+			{number: 19, item: widgets[0]._id.toString()},
 		];
-		users[0].save(function(err, user) {
+		users[1].save(function(err, user) {
 			expect(err).to.not.be.ok;
 			expect(user).to.be.an.object;
 
@@ -65,10 +80,12 @@ describe('Monoxide - save', function() {
 			expect(user).to.have.property('mostPurchased');
 			expect(user.mostPurchased).to.be.an.array;
 			expect(user.mostPurchased).to.have.length(2);
-			expect(user.mostPurchased[0]).to.have.property('number', 12);
-			expect(user.mostPurchased[0].item).to.be.undefined;
-			expect(user.mostPurchased[1]).to.have.property('number', 15);
-			expect(user.mostPurchased[1].item).to.be.undefined;
+			expect(user.mostPurchased[0]).to.have.property('number', 18);
+			expect(user.mostPurchased[0]).to.have.property('item', widgets[1]._id);
+			expect(user.mostPurchased[0].item).to.be.a.string;
+			expect(user.mostPurchased[1]).to.have.property('number', 19);
+			expect(user.mostPurchased[1]).to.have.property('item', widgets[0]._id);
+			expect(user.mostPurchased[1].item).to.be.a.string;
 
 			finish();
 		});
