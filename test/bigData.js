bb5cc4aecd02b50e7617da6b2178fcbd4b68cdec
Matt Carter <m@ttcarter.com>
2018-08-14 16:40:46 +1000

    Big data tests now use iterators

--- test/bigData.js
+++ test/bigData.js
@@ -3,14 +3,13 @@ var expect = require('chai').expect;
 var monoxide = require('..');
 var testSetup = require('./setup');
 
-describe('big data sets', function() {
+describe.skip('big data sets', function() {
+	before(()=> monoxide.use('iterators'));
 	before(testSetup.init);
 	before(testSetup.initFriends);
 	after(testSetup.teardown);
 
-	var friends;
-
-	it('should retrieve a very big data set', function(finish) {
+	it('should retrieve a very big data set into an array', function(done) {
 		this.timeout(120 * 1000);
 
 		monoxide.query({
@@ -18,30 +17,26 @@ describe('big data sets', function() {
 		}, function(err, res) {
 			expect(err).to.not.be.ok;
 			expect(res).to.have.length(10000);
-			friends = res;
-			finish();
+			done();
 		});
 	});
 
-	it('should be able to scope over each item in parallel', function(finish) {
+	it('should be able to scope over each item in parallel using iterators', function(done) {
 		this.timeout(120 * 1000);
-		async()
-			.forEach(friends, function(next, friend) {
+
+		var found = 0;
+		monoxide.models.friends
+			.find()
+			.forEach((next, friend) => {
+				found++;
 				expect(friend).to.have.property('_id');
 				expect(friend).to.have.property('name');
 				next();
 			})
-			.end(finish);
-	});
-
-	it('should be able to perform simple operations in parallel', function(finish) {
-		this.timeout(120 * 1000);
-		async()
-			.forEach(friends, function(next, friend) {
-				friend.username += 'xxx';
-				friend.save(next);
-			})
-			.end(finish);
+			.exec(()=> {
+				expect(found).to.be.equal(10000);
+				done();
+			});
 	});
 
 });
